d0e8a76247a386ee0351e3c811a26abc743ff3ba
Joseph Salisbury <salisbury.joseph@googlemail.com>
2010-07-16 13:25:42 +0100

    Add: Exception handling for decode_int.

--- bencode.py
+++ bencode.py
@@ -2,9 +2,12 @@
 
 # Decode an integer
 def decode_int(data):
-	assert data[0] == "i"	# Check it's an integer
+	try:
+		assert data[0] == "i"	# Check it's an integer
+		end = data.index('e')	# Find the end of the integer
+	except AssertionError, ValueError:
+		raise DecodeError("Badly formed argument")
 
-	end = data.index('e')	# Find the end of the integer
 	# Collapse all the tokens together
 	t = reduce(lambda x, y: x + y, data[1:end])
 
