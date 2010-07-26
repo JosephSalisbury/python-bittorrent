0ed5fb36bef3286db11fe2e139fbcad468bb0f9c
Joseph Salisbury <salisbury.joseph@googlemail.com>
2010-07-26 01:54:07 +0100

    Add: read_torrent_file(), returns a decoded .torrent. Add: make_tracker_request(), not working yet.

--- pytorrent.py
+++ pytorrent.py
@@ -2,12 +2,28 @@
 # pytorrent.py
 
 import bencode
+import urllib
+from hashlib import sha1
 
-# Read the torrent file's contents
-with open("test.torrent") as file:
-	contents = file.read()
+def read_torrent_file(torrent_file):
+	# Read the torrent file's contents
+	with open(torrent_file) as file:
+		contents = file.read()
 
-# Decode the torrent file's contents
-data = bencode.decode(contents)
+	# Decode the torrent file's contents
+	return bencode.decode(contents)
 
-print data.keys()
+def make_tracker_request(torrent):
+	info = sha1( bencode.encode( torrent["info"] )).digest()
+
+	data = {"info_hash" : info}
+	data = urllib.urlencode(data)
+
+	tracker_url = torrent["announce"]
+	response = urllib.urlopen(tracker_url + "?" + data).read()
+
+	print response
+
+def r():
+	t = read_torrent_file("test.torrent")
+	make_tracker_request(t)
