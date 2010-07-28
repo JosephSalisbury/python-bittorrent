e68355c92d292ca2191c80d3d4e888cb375fd786
Joseph Salisbury <salisbury.joseph@googlemail.com>
2010-07-28 21:53:08 +0100

    Add: make_tracker_request() now works, ish. We get peer list, at least.

--- pytorrent.py
+++ pytorrent.py
@@ -23,22 +23,23 @@ class Torrent():
 	def get_info(self):
 		return self.data["info"]
 
-	def tracker_request(self):
+	def make_tracker_request(self):
 		# Hash the info file, for the tracker
 		info = self.get_info()
-		info = sha1(encode(info)).hexdigest()
+		info = sha1(encode(info)).digest()
 
 		# Generate a tracker GET request.
 		payload = {"info_hash" : info,
 				"peer_id" : "ABCDEFGHIJKLMNOPQRST",
-				"port" : 6969,
+				"port" : 6881,
 				"uploaded" : 0,
 				"downloaded" : 0,
-				"left" : None}
+				"left" : 1000,
+				"compact" : 0}
 		payload = urlencode(payload)
 
 		# Send the request
 		tracker_url = self.get_announce_url()
 		response = urlopen(tracker_url + "?" + payload).read()
 
-		self.tracker_response = bencode.decode(response)
+		self.tracker_response = decode(response)
