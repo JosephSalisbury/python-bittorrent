b1b0788b04df2c1e91f5d5d303b64988350ab902
Joseph Salisbury <salisbury.joseph@googlemail.com>
2010-08-09 15:34:30 +0100

    Add: Start of tracker work. Responds to requests.

--- tracker.py
+++ tracker.py
@@ -1,2 +1,22 @@
 # pytorrent-tracker.py
 # A bittorrent tracker
+
+import BaseHTTPServer
+
+HOST = ""
+PORT = 9001
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+	def do_GET(s):
+		print s.path
+
+if __name__ == "__main__":
+	server_class = BaseHTTPServer.HTTPServer
+	httpd = server_class((HOST, PORT), RequestHandler)
+
+	try:
+		httpd.serve_forever()
+	except KeyboardInterrupt:
+		pass
+
+	httpd.server_close()
