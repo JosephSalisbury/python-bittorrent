3fe4c163ac49c7f87b5291b778fedf984d3abd24
Joseph Salisbury <salisbury.joseph@googlemail.com>
2010-08-12 21:02:51 +0100

    Add: Tests for decode_request().

--- tracker_tests.py
+++ tracker_tests.py
@@ -9,8 +9,9 @@ import pickle
 class Read_Torrent_DB(unittest.TestCase):
 	""" Test that we can read the database correctly. """
 
-	def test_reading(self):
+	def test_simple_reading(self):
 		""" Test that a read works correctly. """
+
 		with open("test.txt", "w") as file:
 			pickle.dump("test", file)
 		self.n = tracker.read_torrent_db("test.txt")
@@ -20,10 +21,29 @@ class Read_Torrent_DB(unittest.TestCase):
 class Write_Torrent_DB(unittest.TestCase):
 	""" Test that we can write the database correctly. """
 
-	def test_writing(self):
+	def test_simple_writing(self):
+		""" Test that a write works correctly. """
+
 		tracker.write_torrent_db("test.txt", "test")
 		with open("test.txt") as file:
 			self.n = pickle.load(file)
 		self.assertEqual(self.n, "test")
+
+class Decode_Request(unittest.TestCase):
+	""" Test that we can decode GET requests correctly. """
+
+	def test_simple_request(self):
+		""" Test that a simple request is decoded correctly. """
+
+		self.n = tracker.decode_request("?key=value")
+		self.assertEqual(self.n, {"key":["value"]})
+
+	def test_slash_at_start(self):
+		""" Test that if the request has a slash to start, that it is
+		removed as well. """
+
+		self.n = tracker.decode_request("/?key=value")
+		self.assertEqual(self.n, {"key":["value"]})
+
 
 unittest.main()
