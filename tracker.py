6f498cd7e449ba3f36fc8eddd48e37bc9c1efadc
Joseph Salisbury <salisbury.joseph@googlemail.com>
2010-08-12 14:50:19 +0100

    Refactor: torrents() and interval internalized within Tracker().

--- tracker.py
+++ tracker.py
@@ -8,9 +8,6 @@ from socket import inet_aton
 from struct import pack
 from urlparse import parse_qs
 
-torrents = {}	# Global so RequestHandler can reach it
-INTERVAL = 1800
-
 def read_torrent_db(torrent_db):
 	try:
 		with open(torrent_db, "r") as db:
@@ -33,7 +30,7 @@ def decode_request(path):
 
 	return parse_qs(path)
 
-def add_peer(info_hash, peer_id, ip, port):
+def add_peer(torrents, info_hash, peer_id, ip, port):
 	""" Add the peer to the torrent database. """
 
 	# If we've heard of this, just add the peer
@@ -84,7 +81,6 @@ class Tracker():
 		def do_GET(s):
 			# Decode the request
 			package = decode_request(s.path)
-			print "PACKAGE:", package
 
 			# Get the necessary info out of the request
 			info_hash = package["info_hash"][0]
@@ -93,39 +89,41 @@ class Tracker():
 			port = package["port"][0]
 			peer_id = package["peer_id"][0]
 
-			add_peer(info_hash, peer_id, ip, port)
-
-			print "DB:", torrents
+			add_peer(s.server.torrents, info_hash, peer_id, ip, port)
 
 			# Generate a response
 			response = {}
-			response["interval"] = INTERVAL
+			response["interval"] = s.server.interval
 			response["complete"] = 0
 			response["incomplete"] = 0
-			response["peers"] = peer_list(torrents[info_hash], compact)
-
-			print "RESPONSE:", response
+			response["peers"] = peer_list(s.server.torrents[info_hash], compact)
 
 			# Send off the response
 			s.send_response(200)
 			s.end_headers()
 			s.wfile.write(encode(response))
 
+			print "PACKAGE:", package
+			print "DB:", s.server.torrents
+			print "RESPONSE:", response
 			print
 
-	def __init__(self, host = "", port = 9001, interval = 1800, torrent_db = "tracker.db", inmemory = False):
+	def __init__(self, host = "", port = 9001, interval = 5, torrent_db = "tracker.db", inmemory = False):
 		self.host = host
 		self.port = port
 
 		self.inmemory = inmemory
 
-		if not self.inmemory:	# If not in memory, load the db, otherwise it stays as {}
-			self.torrent_db = torrent_db
-			global torrents
-			torrents = read_torrent_db(self.torrent_db)
-
 		self.server_class = HTTPServer
 		self.httpd = self.server_class((self.host, self.port), self.RequestHandler)
+
+		self.server_class.interval = interval
+
+		if not self.inmemory:	# If not in memory, load the db, otherwise it stays as {}
+			self.torrent_db = torrent_db
+			self.server_class.torrents = read_torrent_db(self.torrent_db)
+		else:
+			self.server_class.torrents = {}
 
 	def run(self):
 		try:
@@ -135,7 +133,7 @@ class Tracker():
 
 	def __del__(self):
 		if not self.inmemory:	# If not in memory, persist the database
-			write_torrent_db(self.torrent_db, torrents)
+			write_torrent_db(self.torrent_db, self.server_class.torrents)
 		self.httpd.server_close()
 
 def main():
